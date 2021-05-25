617a7c2bf4f6ef96bb3995fc482d70fb923c0d26
= <tor@hearkitty.com>
2021-05-25 15:49:55 -0600

    step 1 down

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
 """Flask app for Cupcakes"""
+from flask import Flask, render_template, request, redirect
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+app.config['SECRET_KEY'] = "very_secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+connect_db (app)
+
+@app.route('/')
+def load_home_page():
+    """ load home page """
+    return render_template('home.html')
--- models.py
+++ models.py
@@ -1,1 +1,19 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    """the eponymous Cupcake model"""
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.String, nullable=False)
+    size = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.String, default="https://tinyurl.com/demo-cupcake")
